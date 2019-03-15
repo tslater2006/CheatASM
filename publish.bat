4fc8899b9709631776089b5c992b1fe1b9b446d8
znxDomain <47802543+znxDomain@users.noreply.github.com>
2019-03-14 20:16:40 -0600

    Build MacOS binary also

--- publish.bat
+++ publish.bat
@@ -1,2 +1,3 @@
 dotnet publish -c Release -r linux-x64 --self-contained false
 dotnet publish -c Release -r win10-x64 --self-contained false
+dotnet publish -c Release -r osx-x64 --self-contained false
